3c533dfee75e58069bf92b7e1b27fa9c5007e47a
Aml Fakhri <amlfakhry13@gmail.com>
2022-04-21 22:02:54 +0200

    add new tables.

---
+++ migrations/sqls/20220421134614-001-user-up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE
+  "users"
+ALTER COLUMN
+  password DROP NOT NULL;
--- src/database/tables/user.model.sql
+++ src/database/tables/user.model.sql
@@ -8,3 +8,41 @@ CREATE TABLE users (
   country VARCHAR(150),
   phone VARCHAR(50)
 );
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  price integer NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  status VARCHAR(15),
+  user_id bigint REFERENCES users(id)
+);
+
+CREATE TABLE order_products (
+  id SERIAL PRIMARY KEY,
+  quantity integer,
+  order_id bigint REFERENCES orders(id),
+  product_id bigint REFERENCES products(id)
+);
+
+SELECT
+  *
+FROM
+  products
+ORDER BY
+  price DESC;
+
+SELECT
+  *
+FROM
+  products
+  INNER JOIN order_products ON product.id = order_products.id;
+
+SELECT
+  *
+FROM
+  products
+  INNER JOIN order_products ON product.id = order_products.id;
