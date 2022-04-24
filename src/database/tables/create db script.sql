0705546343a711c1253d214614869a312e12ec0e
Aml Fakhri <amlfakhry13@gmail.com>
2022-04-24 16:21:14 +0200

    fix script.

--- src/database/tables/create db script.sql
+++
@@ -1,5 +0,0 @@
--- Database: store_front
--- DROP DATABASE store_front;
-CREATE DATABASE store_front WITH OWNER = aml_fakhri ENCODING = 'UTF8' LC_COLLATE = 'Arabic_Saudi Arabia.1256' LC_CTYPE = 'Arabic_Saudi Arabia.1256' TABLESPACE = pg_default CONNECTION
-LIMIT
-  = -1;
