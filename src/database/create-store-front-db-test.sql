406a998d90b0a2fa51e8f01b7437ce34eb668fa3
Aml Fakhri <amlfakhry13@gmail.com>
2022-04-26 09:52:25 +0200

    add script for create db test.

---
+++ src/database/create-store-front-db-test.sql
@@ -0,0 +1,5 @@
+-- Database: store_front_test
+-- DROP DATABASE store_front_test;
+CREATE DATABASE store_front_test WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'Arabic_Saudi Arabia.1256' LC_CTYPE = 'Arabic_Saudi Arabia.1256' TABLESPACE = pg_default CONNECTION
+LIMIT
+  = -1;
